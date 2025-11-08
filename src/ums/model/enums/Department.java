c828de78ae39485ad025f35198ba71072cd73013
its_yuhnaa <allanahjae.bragancia@wvsu.edu.ph>
2025-11-08 17:31:59 +0800

    feat(model):add department enum

---
+++ src/ums/model/enums/Department.java
@@ -0,0 +1,16 @@
+package ums.model.enums;
+
+// Enum representing the different academic departments in WVSU - Main Campus
+public enum Department {
+    CAS,   // College of Arts and Sciences
+    CICT,  // College of Information and Communications Technology
+    CBM,   // College of Business & Management
+    COP,   // College of PESCAR
+    COM,   // College of Medicine   
+    COE,   // College of Education
+    CON,   // College of Nursing
+    COC,   // College of Communication
+    COL,   // College of Law
+    COD,   // College of Dentistry
+    ILS    // Integrated Laboratory School
+}
